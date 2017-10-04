69d30b8a1c74456967c47e0f2c5682da24dedc76
Robbert <robbertluit@hotmail.com>
2017-10-04 12:42:18 +0200

    add contact

---
+++ site/src/Project3/WebsiteBundle/Entity/Contact.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Project3\WebsiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contact
+ *
+ * @ORM\Table(name="contact")
+ * @ORM\Entity(repositoryClass="Project3\WebsiteBundle\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="locatie", type="string", length=255)
+     */
+    private $locatie;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="tel", type="string", length=255)
+     */
+    private $tel;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adres", type="string", length=255, nullable=true)
+     */
+    private $adres;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set locatie
+     *
+     * @param string $locatie
+     *
+     * @return Contact
+     */
+    public function setLocatie($locatie)
+    {
+        $this->locatie = $locatie;
+
+        return $this;
+    }
+
+    /**
+     * Get locatie
+     *
+     * @return string
+     */
+    public function getLocatie()
+    {
+        return $this->locatie;
+    }
+
+    /**
+     * Set tel
+     *
+     * @param string $tel
+     *
+     * @return Contact
+     */
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    /**
+     * Get tel
+     *
+     * @return string
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * Set adres
+     *
+     * @param string $adres
+     *
+     * @return Contact
+     */
+    public function setAdres($adres)
+    {
+        $this->adres = $adres;
+
+        return $this;
+    }
+
+    /**
+     * Get adres
+     *
+     * @return string
+     */
+    public function getAdres()
+    {
+        return $this->adres;
+    }
+}
+
