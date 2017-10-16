e0d9d12d15282edee21483f8158066aecd488f5d
Robbert <robbertluit@hotmail.com>
2017-10-16 17:04:00 +0200

    jquery check amount of checked ingredienten

--- site/src/Project3/WebsiteBundle/Resources/public/js/main.js
+++ site/src/Project3/WebsiteBundle/Resources/public/js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
 
+    var telIngredienten = function() {
+
+        var hebik = $( "input:checked" ).length;
+        $( ".shoppinglisttext" ).text( hebik + (hebik === 1 ? " ingredient" : " ingredienten") + " toegevoegd" );
+        alert("Ingredienten die ik heb = " + hebik);
+
+        };
+        $( "#shoppinglistbind" ).on( "click", telIngredienten) ;
+
+});
