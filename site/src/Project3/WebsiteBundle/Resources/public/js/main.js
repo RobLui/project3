b24ca242da664b1fc4a93e00c1b77a0dca844b50
Robbert <robbertluit@hotmail.com>
2017-10-16 18:52:48 +0200

    value changes

--- site/src/Project3/WebsiteBundle/Resources/public/js/main.js
+++ site/src/Project3/WebsiteBundle/Resources/public/js/main.js
@@ -3,10 +3,16 @@ $(document).ready(function() {
     var telIngredienten = function() {
 
         var hebik = $( "input:checked" ).length;
-        $( ".shoppinglisttext" ).text( hebik + (hebik === 1 ? " ingredient" : " ingredienten") + " toegevoegd" );
-        alert("Ingredienten die ik heb = " + hebik);
+        $( ".shoppinglisttext" ).text( hebik + (hebik === 1 ? " ingredient" : " ingredienten") + " in shoppinglijst" );
+        // alert("Ingredienten die ik heb = " + hebik);
+    };
 
-        };
-        $( "#shoppinglistbind" ).on( "click", telIngredienten) ;
+
+
+
+
+
+    // Tel ingredienten die ik heb
+    $( "#shoppinglistbind" ).on( "click", telIngredienten) ;
 
 });
