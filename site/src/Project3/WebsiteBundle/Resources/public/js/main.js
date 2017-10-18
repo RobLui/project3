ef698f454c68524c37480de45fb3708f42d0076f
Robbert <robbertluit@hotmail.com>
2017-10-18 22:16:11 +0200

    working select done -stap- box greyscale

--- site/src/Project3/WebsiteBundle/Resources/public/js/main.js
+++ site/src/Project3/WebsiteBundle/Resources/public/js/main.js
@@ -16,14 +16,24 @@ $(document).ready(function() {
     })
 
 
-    // add box if checked
     $('input:checkbox').change(function(){
-        if($(this).is(":checked")) {
-            $('.stap').addClass("greybox");
-        } else {
-            $('.stap').removeClass("greybox");
-        }
+        if($(this).is(':checked'))
+        // $(this).parent().parent().parent().addClass('greybox');
+        $(this).closest(".col-sm-12").addClass('greybox');
+        else
+        $(this).closest(".col-sm-12").removeClass('greybox')
+        // $(this).parent().parent().parent().removeClass('greybox')
     });
+
+
+    // add box if checked
+    // $('input:checkbox').change(function(){
+    //     if($(this).is(":checked")) {
+    //         $('.stap').addClass("greybox");
+    //     } else {
+    //         $('.stap').removeClass("greybox");
+    //     }
+    // });
 
     //
     // $('input[type=checkbox]').each(function () {
